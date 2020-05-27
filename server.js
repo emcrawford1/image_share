95810310ca3459c0da09b22fb1863e40d479b86a
mrCrawford <noreply>
2020-05-26 23:48:52 -0500

    Trying to fix Cannot GET / error

--- server.js
+++ server.js
@@ -11,7 +11,12 @@ const strategy = require('./config/passport');
 //PORT
 const PORT = process.env.PORT || 3001;
 
-
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"))
+  })
+}
 //Database
 const db = require('./models');
 
