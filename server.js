5d01e30fc93a46ceefb43a4cb0142244dc680653
mrCrawford <noreply>
2020-05-26 23:55:22 -0500

    Trying to fix internal server error.

--- server.js
+++ server.js
@@ -14,8 +14,8 @@ const PORT = process.env.PORT || 3001;
 if(process.env.NODE_ENV === "production"){
   app.use(express.static("build"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "build", "index.html"))
-  })
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+});
 }
 //Database
 const db = require('./models');
