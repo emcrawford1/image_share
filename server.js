4570d2fbb7c68fdb6947a181cd87c1cf24e19bfb
mrCrawford <noreply>
2020-05-28 17:08:01 -0500

    Troubleshooting.

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const PORT = process.env.PORT || 3001;
 
 if (process.env.NODE_ENV === 'production') {
   // Exprees will serve up production assets
-  // app.use(express.static('client/build'));
+  app.use(express.static('client/build'));
 
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
