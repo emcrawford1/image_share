f86237a1cb240f02975e2913267beee952dfa9b8
mrCrawford <noreply>
2020-05-28 13:43:01 -0500

    Troublehootin

--- server.js
+++ server.js
@@ -23,6 +23,8 @@ if (process.env.NODE_ENV === 'production') {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
+
+app.get('*', (req, res) => console.log('Request: ', req))
 //Database
 const db = require('./models');
 
