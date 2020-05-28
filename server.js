00fbb9795d03a6d66bdec040d624d161cafc22e5
mrCrawford <noreply>
2020-05-28 16:42:55 -0500

    Troubleshooting

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ if (process.env.NODE_ENV === 'production') {
   // Express serve up index.html file if it doesn't recognize route
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    console.log('Request: ', req);
   });
 }
 
-app.get('/api/purchaserRoutes/categories', (req, res) => console.log('HEYYYY'))
 //Database
 const db = require('./models');
 
